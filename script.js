d5f294418662501a5a92a14a32555457a8a3d818
KOUAM KAMDEM HERVE <hkouamherve145@gmail.com>
2024-12-31 01:18:14 -0500

    erreur sur les carte

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon")
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+window.addEventListener("scroll", function () {
+  let sections = document.querySelectorAll("section[id]");
+  let navLinks = document.querySelectorAll(".nav-links");
+  let currentSection = "";
+  sections.forEach((section) => {
+    let sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 5) {
+      currentSection = section.getAttribute("id");
     }
+  });
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(currentSection)) {
+      link.classList.add("active");
+    }
+  });
+});
